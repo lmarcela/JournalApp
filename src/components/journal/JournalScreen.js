8ecbe47ab4457186782257ce82d0c44a9d8b2e17
Marcela Malaver <marcela9409@gmail.com>
2020-08-08 22:12:56 -0500

    Estilos del NoteAppBar

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Sidebar } from "./Sidebar";
-import { NothingSelected } from "./NothingSelected";
+import { NoteScreen } from "../notes/NoteScreen";
+//import { NothingSelected } from "./NothingSelected";
 
 export const JournalScreen = () => {
   return (
     <div className="journal__main-content">
       <Sidebar />
       <main>
-        <NothingSelected />
+        {/* <NothingSelected /> */}
+        <NoteScreen />
       </main>
     </div>
   );
