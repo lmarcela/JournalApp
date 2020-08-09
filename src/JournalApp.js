ce8c535606f607088224f4a1017b15a32758e1f3
Marcela Malaver <marcela9409@gmail.com>
2020-08-08 19:37:33 -0500

    Configuracion de rutas principales e hijas

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,10 +1,6 @@
-import React from 'react'
+import React from "react";
+import { AppRouter } from "./routers/AppRouter";
 
 export const JournalApp = () => {
-    return (
-        <div>
-            <h1>JournalApp</h1>
-            <hr />
-        </div>
-    )
-}
+  return <AppRouter />;
+};
