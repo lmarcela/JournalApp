02b87c45e88a53bfa540a8fdc9587c407dbf1b93
Marcela Malaver <marcela9409@gmail.com>
2020-08-08 22:02:39 -0500

    Estilos del NothingSelected

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Sidebar } from './Sidebar'
+import React from "react";
+import { Sidebar } from "./Sidebar";
+import { NothingSelected } from "./NothingSelected";
 
 export const JournalScreen = () => {
-    return (
-        <div className='journal__main-content'>
-            <Sidebar />
-            <main>
-                <h1>Main content</h1>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div className="journal__main-content">
+      <Sidebar />
+      <main>
+        <NothingSelected />
+      </main>
+    </div>
+  );
+};
