5fee8fbaea98cf1de7f0a3ae2f84d270cb342834
Marcela Malaver <marcela9409@gmail.com>
2020-08-10 12:26:43 -0500

    Prueba accion asincrona

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
-import { login } from "../../actions/auth";
+import { startLoginEmailPassword } from "../../actions/auth";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
@@ -13,7 +13,7 @@ export const LoginScreen = () => {
   const { email, password } = formValues;
   const handleLogin = (e) => {
     e.preventDefault();
-    dispatch(login(12345, "Lina"));
+    dispatch(startLoginEmailPassword(email, password));
   };
   return (
     <>
