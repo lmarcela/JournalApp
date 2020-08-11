dfd59b371dd2473abea84a6ec62afeaa42129f40
Marcela Malaver <marcela9409@gmail.com>
2020-08-10 19:26:34 -0500

    Loading state in LoginScreen

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -4,6 +4,15 @@ export const setError = (err) => ({
   type: types.uiSetError,
   payload: err,
 });
+
 export const removeError = () => ({
   type: types.uiRemoveError,
+});
+
+export const startLoading = () => ({
+  type: types.uiStartLoading,
+});
+
+export const finishLoading = () => ({
+  type: types.uiFinishLoading,
 });
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
 import { startLoginEmailPassword, startGoogleLogin } from "../../actions/auth";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.ui);
   const [formValues, handleInputChange] = useForm({
     email: "marcela9409@gmail.com",
     password: "123456",
@@ -41,7 +42,11 @@ export const LoginScreen = () => {
           value={password}
           onChange={handleInputChange}
         />
-        <button className="btn btn-primary btn-block" type="submit">
+        <button
+          className="btn btn-primary btn-block"
+          type="submit"
+          disabled={loading}
+        >
           Login
         </button>
 
