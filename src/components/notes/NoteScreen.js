bfc9c6a8bc564de67dc5eab7f5fba5aaab180cf2
Marcela Malaver <marcela9409@gmail.com>
2020-08-11 20:09:35 -0500

    Activar una nota para su edición

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { NotesAppBar } from "./NotesAppBar";
+import { useSelector } from "react-redux";
+import { useForm } from "../../hooks/useForm";
 
 export const NoteScreen = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange] = useForm(note);
+  const { body, title } = formValues;
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -11,17 +16,23 @@ export const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          value={title}
+          onChange={handleInputChange}
         />
         <textarea
           placeholder="What happened today"
           className="notes__text-area"
+          value={body}
+          onChange={handleInputChange}
         ></textarea>
-        <div className="notes__image">
-          <img
-            src="https://cdn.pixabay.com/photo/2019/12/20/23/07/landscape-4709500_960_720.jpg"
-            alt="imagen"
-          />
-        </div>
+        {note.url && (
+          <div className="notes__image">
+            <img
+              src="https://cdn.pixabay.com/photo/2019/12/20/23/07/landscape-4709500_960_720.jpg"
+              alt="imagen"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
