ec7803327e31955f01a4f8ecd8b5daad8c637745
Marcela Malaver <marcela9409@gmail.com>
2020-08-11 19:24:56 -0500

    Cargar notas de Firestore

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -12,6 +12,11 @@ export const notesReducer = (state = initialState, action) => {
         ...state,
         active: { ...action.payload },
       };
+    case types.notesLoad:
+      return {
+        ...state,
+        notes: [...action.payload],
+      };
 
     default:
       return state;
