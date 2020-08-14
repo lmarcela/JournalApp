7482ab98ea04ced79ace647ba7793fa10787218f
Marcela Malaver <marcela9409@gmail.com>
2020-08-14 18:32:41 -0500

    Inicio pruebas

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,23 @@
+import { types } from "../../types/types";
+
+describe("Pruebas en types.js", () => {
+  test("debe de tener estos tipos", () => {
+    expect(types).toEqual({
+      login: "[Auth] Login",
+      logout: "[Auth] Logout",
+
+      uiSetError: "[UI] Set Error",
+      uiRemoveError: "[UI] Remove Error",
+      uiStartLoading: "[UI] Start loading",
+      uiFinishLoading: "[UI] Finish loading",
+
+      notesAddNew: "[Notes] New note",
+      notesActive: "[Notes] Set active note",
+      notesLoad: "[Notes] Load note",
+      notesUpdated: "[Notes] Updated note",
+      notesFileUrl: "[Notes] Update image url",
+      notesDelete: "[Notes] Delete note",
+      notesLogoutCleaning: "[Notes] Logout Cleaning",
+    });
+  });
+});
