6edb6601d8a2939b2449747e386072c901282ff5
Marcela Malaver <marcela9409@gmail.com>
2020-08-14 12:38:22 -0500

    Borrar una nota

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -2,12 +2,12 @@ import React, { useEffect, useRef } from "react";
 import { NotesAppBar } from "./NotesAppBar";
 import { useSelector, useDispatch } from "react-redux";
 import { useForm } from "../../hooks/useForm";
-import { activeNote } from "../../actions/notes";
+import { activeNote, startDeleting } from "../../actions/notes";
 
 export const NoteScreen = () => {
   const { active: note } = useSelector((state) => state.notes);
   const [formValues, handleInputChange, reset] = useForm(note);
-  const { body, title } = formValues;
+  const { body, title, id } = formValues;
   const activeid = useRef(note.id);
   const dispatch = useDispatch();
 
@@ -21,6 +21,10 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch(activeNote(formValues.id, { ...formValues }));
   }, [formValues, dispatch]);
+
+  const handleDelete = () => {
+    dispatch(startDeleting(id));
+  };
 
   return (
     <div className="notes__main-content">
@@ -48,6 +52,9 @@ export const NoteScreen = () => {
           </div>
         )}
       </div>
+      <button className="btn btn-danger" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 };
